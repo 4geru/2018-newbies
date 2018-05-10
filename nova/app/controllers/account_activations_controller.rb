9e389c4b57d2a98a336a7d1b8f9a52fe2a0f2876
毛 若愚 <ruoyu.mao@icloud.com>
2018-05-10 14:59:25 +0900

    delete comment

--- nova/app/controllers/account_activations_controller.rb
+++ nova/app/controllers/account_activations_controller.rb
@@ -5,13 +5,8 @@ class AccountActivationsController < ApplicationController
     if user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
-<<<<<<< HEAD
-      flash[:success] = "Account activated! Please log in."
-      redirect_to login_url
-=======
       flash[:success] = "アカウントの有効化に成功しました"
       redirect_to dashboard_path
->>>>>>> 2450747b5a2f09d0896d80a0a980a1fc330df9a7
     else
       flash[:alert] = "無効なリンクです"
       redirect_to login_url
