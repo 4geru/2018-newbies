297d4aedf6197695ca790b62548b217eb377a17f
4geru <uchinishi.koichi@moneyforward.co.jp>
2018-05-14 09:16:20 +0900

    add stripe-timeout

--- nova/app/models/charge.rb
+++ nova/app/models/charge.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'timeout'
 
 class Charge < ApplicationRecord
   belongs_to :user
@@ -10,13 +11,19 @@ class Charge < ApplicationRecord
   protected
 
   def create_stripe_charge
-    Stripe::Charge.create(
-      amount: amount,
-      currency: 'jpy',
-      customer: user.stripe_id
-    )
+    Timeout.timeout(3) do
+      sleep(4)
+      Stripe::Charge.create(
+        amount: amount,
+        currency: 'jpy',
+        customer: user.stripe_id
+      )
+    end
   rescue Stripe::StripeError => e
     errors.add(:user, e.code.to_s.to_sym)
+    throw :abort
+  rescue Timeout::Error => e
+    errors.add(:user, e.to_s.to_sym)
     throw :abort
   end
 end
